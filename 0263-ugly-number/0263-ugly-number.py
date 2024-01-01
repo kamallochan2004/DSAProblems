35e71869d14dd59097a18fe10e608df891605210
Kamallochan Das <118735381+kamallochan2004@users.noreply.github.com>
2024-01-01 17:39:58 +0530

    Time: 87 ms (5.06%), Space: 17.2 MB (17.23%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n <= 0:
-            return False
-        while n % 2 == 0:
-            n //= 2
-        while n % 3 == 0:
-            n //= 3
-        while n % 5 == 0:
-            n //= 5
+        for i in 2,3,5:
+            while n % i == 0 < n:
+                n /=i
         return n == 1
